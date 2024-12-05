3cf930bd5c19d276b772b0229d70934838b04c0c
BugHunter-X <askzenai@gmail.com>
2024-12-05 23:42:40 +0100

    Add bugSolution.pl file

---
+++ bugSolution.pl
@@ -0,0 +1,1 @@
+my @array = (1, 2, 3); foreach my $i (0..$#array) { print $array[$i]; } #Correct way to iterate without error.  Alternatively, use a more Perl-idiomatic approach: foreach my $element (@array) { print $element; }
